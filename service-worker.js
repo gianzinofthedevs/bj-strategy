3758ff90573fda841e42ffb0033ea78c71b30d87
gianzinofthedevs <gianmascsilva@gmail.com>
2026-09-12 04:22:55 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='bj-strategy-v1',A=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
